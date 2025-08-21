47bcef5a8a9711cb9f280166471b6c64f36c8c62
kukusha <yeabsira0514@gmail.com>
2025-08-21 11:11:07 +0300

    Enhance Home component styling with Tailwind CSS
    - Updated the layout of the Home component to include a minimum height and padding.
    - Improved the heading style with larger font size, bold weight, and margin for better visual hierarchy.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
 
+
 const Home = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="min-h-screen p-5" >
+      <h1 className="text-3xl font-bold mb-4">Hello World</h1>
       <Button>
         Click me
       </Button>
