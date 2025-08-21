4f72b0b2bb70adacc841fb26a026b231c25c6978
kukusha <yeabsira0514@gmail.com>
2025-08-21 13:38:32 +0300

    Add SignUp page component for user registration
    - Introduced a new SignedUpPage component that utilizes the SignUp component from @clerk/nextjs for user registration.
    - This addition enhances the authentication flow by providing a dedicated page for new users to sign up.

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SignUp } from '@clerk/nextjs'
+
+const SignedUpPage = () => {
+    return (
+        <main className="auth-page">
+            <SignUp />
+        </main>
+    )
+}
+
+export default SignedUpPage
