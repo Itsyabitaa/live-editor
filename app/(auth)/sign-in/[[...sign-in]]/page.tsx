5ff7c9fbf7a0c8dbec00909e9c35efb004b880cb
kukusha <yeabsira0514@gmail.com>
2025-08-21 14:29:47 +0300

    Refactor SignIn page component for improved readability
    - Changed the component definition from a constant to a default export function for better clarity.
    - Simplified the structure while maintaining the same functionality, enhancing code maintainability.

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+
 import { SignIn } from '@clerk/nextjs'
 
-const SignedInPage = () => {
-    return (
-        <main className="auth-page">
-            <SignIn />
-        </main>
-    )
+export default function SignInPage() {
+  return (
+   <main className="auth-page">
+    <SignIn />
+   </main>
+  )
 }
-
-export default SignedInPage
