5253f5c3a4e50015eda1d16af611f4854c53a733
kukusha <yeabsira0514@gmail.com>
2025-08-21 12:29:40 +0300

    Add user authentication buttons to DocumentPage
    - Integrated SignedIn and SignedOut components from Clerk for user authentication.
    - Added SignIn and SignUp buttons to enhance user access and engagement on the DocumentPage.
    - This update improves the user experience by providing clear options for account management.

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignUpButton, SignInButton, UserButton } from '@clerk/nextjs'
 const DocumentPage = () => {
     return (
         <>
@@ -8,6 +9,17 @@ const DocumentPage = () => {
                 <div className='flex w-fit items-center justify-center gap-2'>
                     <p className='document-title'>static tytle</p>
                 </div>
+                <SignedOut>
+                    <SignInButton />
+                    <SignUpButton>
+                        <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                            Sign Up
+                        </button>
+                    </SignUpButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </Header>
             <Editor />
         </>
