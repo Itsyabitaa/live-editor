19c8c8621b6509ba186101ff8fd901033cfc6659
kukusha <yeabsira0514@gmail.com>
2025-08-21 14:30:03 +0300

    Remove SignUp button from DocumentPage for streamlined user authentication experience

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
-import { SignedIn, SignedOut, SignUpButton, SignInButton, UserButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut,  SignInButton, UserButton } from '@clerk/nextjs'
 const DocumentPage = () => {
     return (
         <>
@@ -11,11 +11,6 @@ const DocumentPage = () => {
                 </div>
                 <SignedOut>
                     <SignInButton />
-                    <SignUpButton>
-                        <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                            Sign Up
-                        </button>
-                    </SignUpButton>
                 </SignedOut>
                 <SignedIn>
                     <UserButton />
